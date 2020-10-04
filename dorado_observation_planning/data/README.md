b59cbb4c7ee897c8552f0c2f62e90953305bba99
Leo Singer <leo.singer@ligo.org>
2020-10-04 10:20:43 -0400

    First commit

---
+++ dorado_observation_planning/data/README.md
@@ -0,0 +1,6 @@
+# Dorado data files
+
+orbits.txt
+: selected two-line elements retrieved from
+  https://www.celestrak.com/NORAD/elements/science.txt
+  on Sun Oct  4 09:53:03 EDT 2020.
---
+++ dorado_observation_planning/orbit.py
@@ -0,0 +1,37 @@
+from importlib import resources
+
+from astropy.coordinates import EarthLocation
+from astropy.time import Time
+from astropy import units as u
+import skyfield.api
+
+from . import data
+
+__all__ = ('orbit',)
+
+
+# Load two-line element for satellite (use Swift's orbit)
+with resources.path(data, 'orbits.txt') as path:
+    TLE = skyfield.api.load.tle(str(path))['SWIFT']
+
+TIMESCALE = skyfield.api.load.timescale()
+
+
+def position(time):
+    """Get the position of the satellite.
+
+    Parameters
+    ----------
+    satellite : skyfield.sgp4lib.EarthSatellite
+        An Earth satellite model from Skyfield.
+    time : astropy.time.Time, skyfield.timelib.Timeto
+        The time of the observation.
+
+    Returns
+    -------
+    earth_location : astropy.coordinates.EarthLocation
+        The geocentric position of the satellite.
+    """
+    if isinstance(time, astropy.time.Time):
+        time = TIMESCALE.from_astropy(time)
+    return EarthLocation.from_geocentric(*TLE.at(time).position.to(u.meter))
