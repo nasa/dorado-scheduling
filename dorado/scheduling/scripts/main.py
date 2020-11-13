7040da2485b1981157237990463ac7dda45bb11a
Leo Singer <leo.singer@ligo.org>
2020-11-13 09:47:52 -0500

    Port to CPLEX

--- dorado/scheduling/scripts/main.py
+++ dorado/scheduling/scripts/main.py
@@ -11,8 +11,6 @@ def parser():
     p.add_argument('skymap', metavar='FILE.fits[.gz]',
                    type=FileType('rb'), help='Input sky map')
     p.add_argument('-n', '--nexp', type=int, help='Number of exposures')
-    p.add_argument('--max-seconds', type=int, default=300,
-                   help='Time limit for solver')
     p.add_argument('--output', '-o', metavar='OUTPUT.ecsv',
                    type=FileType('w'), default='-',
                    help='output filename')
@@ -31,10 +29,10 @@ def main(args=None):
     from astropy.io import fits
     from astropy.time import Time
     from astropy.table import Table
+    from docplex.mp.model import Model
     from ligo.skymap.io import read_sky_map
     from ligo.skymap.bayestar import rasterize
     from ligo.skymap.util import Stopwatch
-    import mip
     import numpy as np
     from scipy.signal import convolve
     from tqdm import tqdm
@@ -44,7 +42,6 @@ def main(args=None):
     from .. import skygrid
 
     log.info('reading sky map')
-
     # Read multi-order sky map and rasterize to working resolution
     start_time = Time(fits.getval(args.skymap, 'DATE-OBS', ext=1))
     skymap = read_sky_map(args.skymap, moc=True)['UNIQ', 'PROBDENSITY']
@@ -55,37 +52,47 @@ def main(args=None):
     times = np.arange(orbit.time_steps) * orbit.time_step_duration + start_time
 
     log.info('generating model')
-    m = mip.Model()
+    m = Model()
 
     log.info('adding variable: observing schedule')
     shape = (len(skygrid.centers), len(skygrid.rolls),
              orbit.time_steps - orbit.time_steps_per_exposure + 1)
-    schedule = m.add_var_tensor(shape, name='p', var_type=mip.BINARY)
+    schedule = np.reshape(m.binary_var_list(np.prod(shape)), shape)
 
     log.info('adding variable: whether a given pixel is observed')
-    pixel_observed = m.add_var_tensor(
-        (skygrid.healpix.npix,), name='s', var_type=mip.BINARY)
+    pixel_observed = np.asarray(m.binary_var_list(skygrid.healpix.npix))
 
     log.info('adding variable: whether a given field is used')
-    field_used = m.add_var_tensor(shape[:2], name='f', var_type=mip.BINARY)
+    field_used = np.reshape(m.binary_var_list(np.prod(shape[:2])), shape[:2])
 
     log.info('adding variable: whether a given time step is used')
-    time_used = m.add_var_tensor((shape[2],), name='t', var_type=mip.BINARY)
+    time_used = np.asarray(m.binary_var_list(shape[2]))
 
     if args.nexp is not None:
         log.info('adding constraint: number of exposures')
-        m += mip.xsum(time_used) <= 0
+        m.add_constraint_(m.sum(time_used) <= args.nexp)
 
     log.info('adding constraint: only observe one field at a time')
-    for i in tqdm(range(shape[2])):
-        m += mip.xsum(schedule[..., i].ravel()) == time_used[i]
-        m += mip.xsum(time_used[i:i+orbit.time_steps_per_exposure]) <= 1
+    m.add_constraints_(
+        m.sum(schedule[..., i].ravel()) <= 1 for i in tqdm(range(shape[2]))
+    )
+    m.add_equivalences(
+        time_used,
+        [m.sum(schedule[..., i].ravel()) >= 1 for i in tqdm(range(shape[2]))]
+    )
+    m.add_constraints_(
+        m.sum(time_used[i:i+orbit.time_steps_per_exposure]) <= 1
+        for i in tqdm(range(schedule.shape[-1]))
+    )
 
     log.info('adding constraint: a pixel is observed if it is in any field')
-    for lhs, rhs in zip(
+    m.add_constraints_(
+        m.sum(lhs) >= rhs
+        for lhs, rhs in zip(
             tqdm(schedule.reshape(field_used.size, -1)),
-            field_used.ravel()):
-        m += mip.xsum(lhs) >= rhs
+            field_used.ravel()
+        )
+    )
     indices = [[] for _ in range(skygrid.healpix.npix)]
     with tqdm(total=len(skygrid.centers) * len(skygrid.rolls)) as progress:
         for i, grid_i in enumerate(skygrid.get_footprint_grid()):
@@ -93,8 +100,10 @@ def main(args=None):
                 for k in grid_ij:
                     indices[k].append((i, j))
                 progress.update()
-    for lhs_indices, rhs in zip(tqdm(indices), pixel_observed):
-        m += mip.xsum(field_used[inds] for inds in lhs_indices) >= rhs
+    m.add_constraints_(
+        m.sum(field_used[lhs_index] for lhs_index in lhs_indices) >= rhs
+        for lhs_indices, rhs in zip(tqdm(indices), pixel_observed)
+    )
 
     log.info('adding constraint: field of regard')
     i, j = np.nonzero(
@@ -102,25 +111,28 @@ def main(args=None):
             ~get_field_of_regard(times),
             np.ones(orbit.time_steps_per_exposure)[:, np.newaxis],
             mode='valid', method='direct'))
-    m += mip.xsum(schedule[j, :, i].ravel()) <= 0
+    m.add_constraint_(m.sum(schedule[j, :, i].ravel()) <= 0)
 
     log.info('adding objective')
-    m.objective = mip.maximize(mip.xsum(prob * pixel_observed))
+    m.maximize(m.scal_prod(pixel_observed, prob))
 
     log.info('solving')
     stopwatch = Stopwatch()
     stopwatch.start()
-    m.optimize(max_seconds=args.max_seconds)
+    solution = m.solve(log_output=True)
     stopwatch.stop()
 
     log.info('extracting results')
-    if m.status in {mip.OptimizationStatus.FEASIBLE,
-                    mip.OptimizationStatus.OPTIMAL}:
-        schedule_flags = schedule.astype(float).astype(bool)
-        objective_value = m.objective_value
-    else:
+    if solution is None:
         schedule_flags = np.zeros(schedule.shape, dtype=bool)
         objective_value = 0.0
+    else:
+        schedule_flags = np.asarray(
+            solution.get_values(schedule.ravel()), dtype=bool
+        ).reshape(
+            schedule.shape
+        )
+        objective_value = m.objective_value
 
     ipix, iroll, itime = np.nonzero(schedule_flags)
     result = Table(
@@ -132,7 +144,7 @@ def main(args=None):
             # FIXME: use shlex.join(sys.argv) in Python >= 3.8
             'cmdline': ' '.join(sys.argv),
             'prob': objective_value,
-            'status': m.status.name,
+            'status': m.solve_status.name,
             'real': stopwatch.real,
             'user': stopwatch.user,
             'sys': stopwatch.sys
