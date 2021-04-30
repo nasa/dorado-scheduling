c64a0dcb3acaa018c321ad7f6b2feb4155df3c68
Leo Singer <leo.singer@ligo.org>
2021-04-29 21:25:39 -0400

    Catch one more test coverage line

--- dorado/scheduling/tests/test_skygrid.py
+++ dorado/scheduling/tests/test_skygrid.py
@@ -29,3 +29,9 @@ geodesic_methods = [
 def test_skygrid(method, area):
     coords = method(area)
     assert len(coords) >= (u.spat / area)
+
+
+def test_invalid_geodesic_polyhedron():
+    with pytest.raises(ValueError):
+        # No such thing as a class IV geodesic polyhedron
+        skygrid.geodesic(10 * u.deg**2, class_='IV')
