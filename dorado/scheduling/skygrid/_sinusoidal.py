137a4f3cd765f198dbc3595177eaf758f2835c7d
Leo Singer <leo.singer@ligo.org>
2021-05-04 17:20:15 -0400

    Fill gaps in sinusoidal tiling near poles
    Instead of calculating the number of tiles in a given ring from its
    central declination, use the declination of the edge of the ring
    that is closest to the equator.
    
    This dramatically improves tiling efficiency near the poles.

--- dorado/scheduling/skygrid/_sinusoidal.py
+++ dorado/scheduling/skygrid/_sinusoidal.py
@@ -14,7 +14,8 @@ def sinusoidal(area):
     """Generate a uniform grid on a sinusoidal equal area projection.
 
     This is similar to what was used for GRANDMA follow-up in LIGO/Virgo
-    Observing Run 3 (O3). See :doi:`10.3847/2041-8213/ab3399`.
+    Observing Run 3 (O3), but is more efficient at tiling the poles.
+    See :doi:`10.3847/2041-8213/ab3399`.
 
     Parameters
     ----------
@@ -39,10 +40,12 @@ def sinusoidal(area):
     # Declinations of equal-declination strips
     n_decs = int(np.ceil(np.pi / width)) + 1
     decs = np.linspace(-0.5 * np.pi, 0.5 * np.pi, n_decs)
+    d_dec = decs[1] - decs[0]
 
     # Number of RA steps in each equal-declination strip
-    n_ras = np.ceil(2 * np.pi * np.cos(decs) / width).astype(int)
-    n_ras = np.maximum(1, n_ras)
+    decs_edge = decs - 0.5 * d_dec * np.where(decs >= 0, 1, -1)
+    n_ras = np.ceil(2 * np.pi * np.cos(decs_edge) / width).astype(int)
+    n_ras[0] = n_ras[-1] = 1
 
     ras = np.concatenate([np.linspace(0, 2 * np.pi, n, endpoint=False)
                           for n in n_ras])
