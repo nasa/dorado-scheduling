5f1a3ea4df05ea6ecfcc84fc6a5e19312f7c2b6b
Leo Singer <leo.singer@ligo.org>
2021-05-04 17:37:32 -0400

    Plot sky grid at celestial south pole

--- examples/plot_skygrid.py
+++ examples/plot_skygrid.py
@@ -25,7 +25,8 @@ verts = np.moveaxis(np.stack([footprints.ra.deg, footprints.dec.deg]), 0, -1)
 
 for kw in [dict(projection='astro globe', center='0d 30d'),
            dict(projection='astro zoom', center='0d 0d', radius='30 deg'),
-           dict(projection='astro zoom', center='0d 90d', radius='30 deg')]:
+           dict(projection='astro zoom', center='0d 90d', radius='30 deg'),
+           dict(projection='astro zoom', center='0d -90d', radius='30 deg')]:
     fig, ax = plt.subplots(subplot_kw=kw)
     ax.grid()
     transform = ax.get_transform('world')
